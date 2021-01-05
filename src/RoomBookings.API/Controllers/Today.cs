a46e76891919169bf3b4761a35cea65ab8c77e81
Mark Strendin <mark.strendin@gmail.com>
2021-01-05 16:09:39 -0600

    Tinker with the default API code a bit

---
+++ src/RoomBookings.API/Controllers/Today.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LivingSky.RoomBookings;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RoomBookings.API.Controllers
+{
+    [ApiController]
+    [Route("/today")]
+    public class TodayController : ControllerBase
+    {
+        private readonly ILogger<TodayController> _logger;
+
+        public TodayController(ILogger<TodayController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<IBooking> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new SingleBooking()
+            {
+                
+            })
+            .ToArray();
+        }
+    }
+}
